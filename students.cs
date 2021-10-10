248fa81b65c5a64be6ac110a21a976fc36f63f74
oleksiimatsui <92246422+oleksiimatsui@users.noreply.github.com>
2021-10-10 11:01:27 +0300

    Update students.cs

--- students.cs
+++ students.cs
@@ -2,35 +2,78 @@ using System;
 using System.Collections.Generic;
 using Microsoft.VisualBasic.FileIO;
 
-namespace Ok
+namespace Student
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("please, input some numeric\n");
-            string input = Console.ReadLine();
-            double num = Convert.ToDouble(input);
-            Converter superconv = new Converter(26.28, 30.36);
-            Console.Write(num+" grn is " + superconv.GrnToEur(num) + " eur\n");
-            Console.Write(num+" eur is " + superconv.EurToGrn(num) + " grn\n");
-            Console.Write(num+" grn is " + superconv.GrnToUsd(num) + " usd\n");
-            Console.Write(num+" usd is " + superconv.UsdToGrn(num) + " grn\n");
+            Group group1 = new Group("K-25");
+            string name1 = "Semen";
+            BadStudent st1 = new BadStudent(name1);
+            GoodStudent st2 = new GoodStudent(name1);
+            BadStudent st3 = new BadStudent(name1);
+            group1.AddStudent(st1);
+            group1.AddStudent(st2);
+            group1.AddStudent(st3);
+            st3.Study();
+            st2.Study();
+            group1.GetInfo();
+            group1.GetFullInfo();
         }
-        public class Converter
+    }
+    class Group
+    {
+        public Group(string _name)
+        { name = _name; }
+        public void AddStudent(Student st)
         {
-            public Converter(double usd, double eur)
-            {
-                usd_course = usd;
-                eur_course = eur;
-            }
-            public double GrnToEur(double a) { return a / eur_course; }
-            public double EurToGrn(double a) { return a * eur_course; }
-            public double GrnToUsd(double a) { return a / usd_course; }
-            public double UsdToGrn(double a) { return a * usd_course; }
-
-            private double usd_course;
-            private double eur_course;
+            list.Add(st);
         }
+        public void GetInfo()
+        {
+            Console.WriteLine("Group name: " + name);
+            Console.WriteLine("Students: ");
+            foreach (Student student in list) Console.Write(student.GetName() + " ");
+        }
+        public void GetFullInfo()
+        {
+            Console.WriteLine("Group name: " + name);
+            Console.WriteLine("Students: ");
+            foreach (Student student in list) { 
+                Console.Write(student.GetName() + " "); 
+                Console.Write(student.GetState() + "\n");   
+            };
+        }
+        private string name;
+        private List<Student> list = new List<Student>(3);
+    }
+    abstract class Student
+    {
+        private string name;
+        protected string state;
+        protected void Read() { state += "read "; }
+        protected void Write() { state += "write "; }
+        protected void Relax() { state += "relax "; }
+        public Student(string _name) { name = _name; state = ""; }
+        public abstract void Study();
+        public string GetName() { return name; }
+        public string GetState() { return state; }
+    }
+    class GoodStudent : Student
+    {
+        public GoodStudent(string _name) : base(_name) 
+        {
+            state = "good "; 
+        }
+        override public void Study() { Read(); Write(); Read(); Write(); Relax(); }
+    }
+    class BadStudent : Student
+    {
+        public BadStudent(string _name) : base(_name)
+        {
+            state = "bad ";
+        }
+        override public void Study() { Relax(); Relax(); Relax(); Relax(); Read(); }
     }
 }
