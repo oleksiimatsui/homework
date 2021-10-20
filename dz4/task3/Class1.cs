3223fb198e04fe500f48b00c4a5660220cf16e61
oleksiimatsui <92246422+oleksiimatsui@users.noreply.github.com>
2021-10-20 16:50:57 +0300

    Add files via upload

---
+++ dz4/task3/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    abstract public class Triangle
+    {
+        protected double A;
+        protected double B;
+        protected double Angle;
+        virtual public double CalcPer(double a, double b, double angle) 
+        {
+            return a + b + Math.Sqrt(a * a + b * b - 2 * a * b * Math.Cos(angle));
+        }
+        virtual public double CalcScale(double a, double b, double angle)
+        {
+            return a * b * Math.Sin(angle);
+        }
+    }
+
+    public class RightTriangle:Triangle
+    {
+        public RightTriangle(double a, double b)
+        {
+            A = a;
+            B = b;
+            Angle = 90;
+        }
+        public double GetPer()
+        {
+            return base.CalcPer(A, B, 90);
+        }
+        public double GetScale()
+        {
+            return base.CalcScale(A, B, 90);
+        }
+
+    }
+
+    public class EquilateralTriangle:Triangle
+    {
+        public EquilateralTriangle(double a)
+        {
+            A = a;
+            B = a;
+            Angle = 60;
+        }
+
+        public double GetPer()
+        {
+            return base.CalcPer(A, A, 60);
+        }
+        public double GetScale()
+        {
+            return base.CalcScale(A, A, 60);
+        }
+    }
+}
---
+++ dz4/task3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            RightTriangle rt = new RightTriangle(10,20);
+            EquilateralTriangle et = new EquilateralTriangle(15);
+            
+            output.Text += rt.GetScale().ToString() + "\n";
+            output.Text += rt.GetPer().ToString() + "\n";
+
+            output.Text += et.GetScale().ToString() + "\n";
+            output.Text += et.GetPer().ToString() + "\n";
+        }
+
+    }
+}
