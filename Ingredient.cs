0219d31940905f7629619f3c68042727539d85d2
oleksiimatsui <92246422+oleksiimatsui@users.noreply.github.com>
2022-03-03 16:56:48 +0200

    Add files via upload

---
+++ Ingredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DiningRoomWebApplication
+{
+    public partial class Ingredient
+    {
+        public Ingredient()
+        {
+            DishIncludes = new HashSet<DishInclude>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<DishInclude> DishIncludes { get; set; }
+    }
+}
